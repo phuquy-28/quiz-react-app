b51981c1b4b07038b0fc69ceaddc36ce697becb6
phuquy-28 <90885392+phuquy-28@users.noreply.github.com>
2024-04-21 00:52:08 +0700

    create login ui

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -1,5 +1,44 @@
+import "./Login.scss";
+import { useState } from "react";
 const Login = () => {
-  return <>login page</>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const handleSubmit = () => {
+    alert(email + " " + password);
+  };
+  return (
+    <div className="login-container">
+      <div className="header">Don't have an account yet?</div>
+      <div className="title col-4 mx-auto">Quizz App</div>
+      <div className="welcome col-4 mx-auto">Hello, who's this?</div>
+      <div className="content-form col-4 mx-auto d-flex flex-column gap-3">
+        <div className="form-group">
+          <label>Email</label>
+          <input
+            type="email"
+            className="form-control"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div className="form-group">
+          <label>Password</label>
+          <input
+            type="password"
+            className="form-control"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <span className="forgot-password">Forget password</span>
+        <div>
+          <button className="btn-submit" onClick={() => handleSubmit()}>
+            Login
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
